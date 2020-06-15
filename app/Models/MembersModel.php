963996b8262ec62323bfc598a31189e7dd7f1e4f
sharminex81 <mimetyshanta@gmail.com>
2020-06-15 22:18:10 +0600

    List, view, update are done.

--- app/Models/MembersModel.php
+++ app/Models/MembersModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
@@ -45,8 +46,8 @@ class MembersModel extends Model
      */
     public static function getAll()
     {
-        $result = Cache::remember('get_members_cache', 60 * 60 * 24, function (){
-            self::get();
+        $result = Cache::remember('get_members_cache', 10, function (){
+            return self::get();
         });
 
         return $result;
@@ -82,5 +83,38 @@ class MembersModel extends Model
         });
 
         return $result;
+    }
+
+    /**
+     * @param $postData
+     * @return bool
+     */
+    public function updateMember($postData)
+    {
+        //Cache::forget('get_member_id');
+        $getData = Redis::get('get_member_id');
+
+        if(!empty($getData) || $getData !== null){
+            DB::connection()->enableQueryLog();
+            $member = $this->find($postData->member_id);
+            $log = DB::getQueryLog();
+            //print_r($log);
+
+            Redis::set('get_member_id', $member->id);
+
+            $memberInfo = $this->find($member->id);
+            $memberInfo->first_name = $postData->first_name;
+            $memberInfo->last_name = $postData->last_name;
+            $memberInfo->gmail_address = $postData->gmail_address;
+            $memberInfo->role = $postData->role;
+            $memberInfo->location = $postData->location;
+
+            if ($memberInfo->save() === true) {
+                Redis::set('get_member_details', $memberInfo);
+                return true;
+            }
+
+            return false;
+        }
     }
 }
