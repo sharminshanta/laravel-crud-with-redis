4258fdbb24b9f4f81519d09af3224da1a9bb9376
sharminex81 <mimetyshanta@gmail.com>
2020-06-15 22:17:30 +0600

    All style file are local now.

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -4,10 +4,10 @@
     <title>TMSS | @yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('/')}}assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="{{asset('/')}}assets/js/jquery.min.js"></script>
+    <script src="{{asset('/')}}assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 
